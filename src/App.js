eea4259301ed706cd4b4fde18ae9003061bc03f7
Evan Zhu <evanzhu888@gmail.com>
2024-02-11 01:11:44 -0500

    implement settings line num functionality

--- src/App.js
+++ src/App.js
@@ -8,16 +8,17 @@ function App() {
   const [content, setContent] = useState("");
   const [currentFileHandle, setCurrentFileHandle] = useState();
   const [currentFileName, setCurrentFileName] = useState();
+  const [showLineNumbers, setShowLineNumbers] = useState(true);
 
   return (
     <div className="App">
-      <Sidebar content={content} setContent={setContent} currentFileHandle={currentFileHandle} setCurrentFileHandle={setCurrentFileHandle} setCurrentFileName={setCurrentFileName}
+      <Sidebar content={content} setContent={setContent} currentFileHandle={currentFileHandle} setCurrentFileHandle={setCurrentFileHandle} setCurrentFileName={setCurrentFileName} showLineNumbers={showLineNumbers} setShowLineNumbers={setShowLineNumbers}
       />
       <div className="vertical-container">
         <Toolbar currentFileName={currentFileName}
         ></Toolbar>
         <CodeEditor
-          setContent={setContent} content={content}
+          setContent={setContent} content={content} showLineNumbers={showLineNumbers}
         ></CodeEditor>
       </div>
     </div>
