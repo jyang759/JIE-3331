a954c1bd7ad7e618a03eeebb5be4a9b232ca67f7
jyang759 <jyang759@gatech.edu>
2024-03-06 20:36:01 -0500

    modified:   package-lock.json 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './App.css';
-import CodeEditor from './components/CodeEditor.js';
+import CodeEditor from './components/CodeEditor';
 import Toolbar from './components/Toolbar';
-import { Sidebar } from './components/sidebar.js';
+import { Sidebar } from './components/sidebar';
 
 let counter = 0;
 
 function App() {
-
-  //Text content 
   const [currentContent, setContent] = useState("");
-
-  //File states for saving files
   const [currentFileHandle, setCurrentFileHandle] = useState();
-  const [currentFileName, setCurrentFileName] = useState();
-
-  //Setting states for settings
+  const [currentFileName, setCurrentFileName] = useState("");
   const [showLineNumbers, setShowLineNumbers] = useState(true);
   const [tabSize, setTabSize] = useState(4);
   const [fontSize, setFontSize] = useState(15);
   const [fontColor, setFontColor] = useState('#000000');
   const [spellCheck, setSpellCheck] = useState(false);
-
-  const [openFiles, setOpenFiles] = useState([
-    {
-      name: `Untitled-${counter}`, 
-      content: "",
-      id: counter
-    }
-  ]);
-
+  const [openFiles, setOpenFiles] = useState([{ name: `Untitled-${counter}`, content: "", id: counter }]);
   const [activeTab, setActiveTab] = useState(counter);
 
   useEffect(() => {
@@ -39,95 +25,82 @@ function App() {
     } else {
       setContent("");
     }
-  }, [activeTab, openFiles])
+  }, [activeTab, openFiles]);
 
-  function switchTab(tabID){
+  const switchTab = useCallback((tabID) => {
     setOpenFiles(prevFiles => {
-    const fileToUpdateIndex = prevFiles.findIndex(file => file.id === activeTab);
-    if (fileToUpdateIndex !== -1) {
-      const fileToUpdate = prevFiles[fileToUpdateIndex];
-      const newFiles = prevFiles;
-      newFiles.splice(fileToUpdateIndex, 1, {
-          ...fileToUpdate,
-        content: currentContent
-      })
-        return newFiles;
-    }
-    return prevFiles;
-  })
+      return prevFiles.map(file => file.id === activeTab ? { ...file, content: currentContent } : file);
+    });
     setActiveTab(tabID);
-  }
+  }, [activeTab, currentContent]);
 
-  function addToOpenFiles(){
-    const newFile = {
-      name: `Untitled-${counter+1}`,
-      content: "",
-      id: ++counter
-    };
+  const addToOpenFiles = useCallback(() => {
+    const newID = ++counter;
+    const newFile = { name: `Untitled-${newID}`, content: "", id: newID };
     setOpenFiles(prevFiles => [...prevFiles, newFile]);
-    setActiveTab(newFile.id);
-  }
+    setActiveTab(newID);
+  }, []);
 
-  function closeTab(tabID) {
+  const closeTab = useCallback((tabID) => {
     setOpenFiles(prevFiles => {
-      const remainingFiles = openFiles.filter(file => file.id !== tabID);
-      if (remainingFiles.length === 0) { //this is defaulted but we need to find a way to deal when all the tabs are deleted
-        remainingFiles.push({
-          name: "deleted all tabs!",
-          content: "",
-          id: ++counter
-        });
-        setActiveTab(remainingFiles[0].id);
+      const remainingFiles = prevFiles.filter(file => file.id !== tabID);
+      if (remainingFiles.length === 0) {
+        const newID = ++counter;
+        const newTab = { name: `Untitled-${newID}`, content: "", id: newID };
+        remainingFiles.push(newTab);
+        setActiveTab(newID);
       } else {
-        if (prevFiles.length === 1 && prevFiles[0].name === "deleted all tabs!") {
-          setActiveTab(prevFiles[0].id);
-        } else {
-          const closeTabIndex = prevFiles.findIndex(file => file.id === tabID);
-          if(tabID === activeTab) {
-          const nextActiveTabIndex = closeTabIndex > 0 ? closeTabIndex - 1 : 0;
-          //console.log(prevFiles)
-          setActiveTab(prevFiles[nextActiveTabIndex].id);
-          }
+        if (tabID === activeTab) {
+          const newActive = remainingFiles[0].id;
+          setActiveTab(newActive);
         }
       }
       return remainingFiles;
     });
-  }
+  }, [activeTab]);
 
   return (
     <div className="App">
-      <Sidebar content={currentContent} setContent={setContent} 
-      currentFileHandle={currentFileHandle} setCurrentFileHandle={setCurrentFileHandle} setCurrentFileName={setCurrentFileName} 
-
-      // settings
-      showLineNumbers={showLineNumbers} setShowLineNumbers={setShowLineNumbers}
-      tabSize={tabSize} setTabSize={setTabSize}
-      fontSize={fontSize} setFontSize={setFontSize}
-      fontColor={fontColor} setFontColor={setFontColor}
-      spellChecking={spellCheck} setSpellChecking={setSpellCheck}
+      <Sidebar 
+        content={currentContent} 
+        setContent={setContent}
+        currentFileHandle={currentFileHandle} 
+        setCurrentFileHandle={setCurrentFileHandle} 
+        setCurrentFileName={setCurrentFileName}
+        showLineNumbers={showLineNumbers} 
+        setShowLineNumbers={setShowLineNumbers}
+        tabSize={tabSize} 
+        setTabSize={setTabSize}
+        fontSize={fontSize} 
+        setFontSize={setFontSize}
+        fontColor={fontColor} 
+        setFontColor={setFontColor}
+        spellChecking={spellCheck} 
+        setSpellChecking={setSpellCheck}
       />
       <div className="vertical-container">
-        <Toolbar currentFileName={currentFileName}
-                  currentFileHandle={currentFileHandle} 
-                  setCurrentFileHandle={setCurrentFileHandle} 
-                  setCurrentFileName={setCurrentFileName}
-                  setContent={setContent}
-                  openFiles={openFiles}
-                  addToOpenFiles={addToOpenFiles}
-                  setActiveTab={switchTab}
-                  activeTab={activeTab}
-                  setOpenFiles={setOpenFiles}
-                  closeTab={closeTab}
-        ></Toolbar>
+        <Toolbar 
+          currentFileName={currentFileName}
+          currentFileHandle={currentFileHandle} 
+          setCurrentFileHandle={setCurrentFileHandle} 
+          setCurrentFileName={setCurrentFileName}
+          setContent={setContent}
+          openFiles={openFiles}
+          addToOpenFiles={addToOpenFiles}
+          setActiveTab={switchTab}
+          activeTab={activeTab}
+          setOpenFiles={setOpenFiles}
+          closeTab={closeTab}
+        />
         <CodeEditor
-          setContent={setContent} content={currentContent}
-          //settings
+          setContent={setContent} 
+          content={currentContent}
           showLineNumbers={showLineNumbers}
           resizeTabSize={tabSize}
           settingsFontSize={fontSize}
           settingsFontColor={fontColor}
           spellCheckOn={spellCheck}
-        ></CodeEditor>
+        />
       </div>
     </div>
   );
