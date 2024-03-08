4ba0dcad5b4fa2fe399bab92378201c43703edfc
jyang759 <jyang759@gatech.edu>
2024-03-08 13:18:25 -0500

    modified:   src/components/ColorPicker.js

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -2,40 +2,40 @@ import React, { useState } from 'react';
 import { SketchPicker } from 'react-color';
 
 export default function ColorPicker({ onColorChange }) {
-    const [showColorPicker, setShowColorPicker] = useState(false);
-    const [selectedColor, setSelectedColor] = useState('#000000'); // Initial color
+  const [showColorPicker, setShowColorPicker] = useState(false);
+  const [selectedColor, setSelectedColor] = useState('#000000'); // Initial color
 
-    const handleColorChange = (color) => {
-        setSelectedColor(color.hex);
-        onColorChange(color.hex);
-    };
+  const handleColorChange = (color) => {
+    setSelectedColor(color.hex);
+    onColorChange(color.hex);
+  };
 
-    const toggleColorPicker = () => {
-        setShowColorPicker(!showColorPicker);
+  const toggleColorPicker = () => {
+    setShowColorPicker(!showColorPicker);
+  };
 
-    };
-
-    return (
-        <div>
-            <div
-                style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    cursor: 'pointer',
-                }}
-                onClick={toggleColorPicker}
-            >Color
-            </div>
-            {showColorPicker && (
-                <div
-                    style={{
-                        position: 'absolute',
-                        zIndex: 2,
-                    }}
-                >
-                    <SketchPicker color={selectedColor} onChange={handleColorChange} />
-                </div>
-            )}
+  return (
+    <div>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          cursor: 'pointer',
+        }}
+        onClick={toggleColorPicker}
+      >
+        Color
+      </div>
+      {showColorPicker && (
+        <div
+          style={{
+            position: 'absolute',
+            zIndex: 2,
+          }}
+        >
+          <SketchPicker color={selectedColor} onChange={handleColorChange} />
         </div>
-    );
-};
+      )}
+    </div>
+  );
+}
