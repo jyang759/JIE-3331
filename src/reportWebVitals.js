e08793a0d14f8294cffc9ea4a166239f75643cf1
jyang759 <jyang759@gatech.edu>
2024-04-15 19:43:06 -0400

    modified:   src/reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,12 +1,20 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+const reportWebVitals = (callbacks) => {
+  if (callbacks && typeof callbacks === 'object') {
+    import('web-vitals')
+      .then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+        if (callbacks.getCLS) getCLS(callbacks.getCLS);
+        if (callbacks.getFID) getFID(callbacks.getFID);
+        if (callbacks.getFCP) getFCP(callbacks.getFCP);
+        if (callbacks.getLCP) getLCP(callbacks.getLCP);
+        if (callbacks.getTTFB) getTTFB(callbacks.getTTFB);
+
+        console.log('Web vitals have been successfully reported.');
+      })
+      .catch((error) => {
+        console.error('Error loading web vitals:', error);
+      });
+  } else {
+    console.warn('No valid callback functions provided for reporting web vitals.');
   }
 };
 
