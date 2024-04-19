1f4768d8b7e0dae717bbd15eb4857513371b68ca
JackXuCatch22 <jackxumarch14@gmail.com>
2024-04-18 21:48:32 -0400

    Created other sections for options

---
+++ src/components/TextFormatingOptions.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './Dropdown.css';
+
+function TextFormatingOptions ({
+    showLineNumbers,
+    setShowLineNumbers,
+    tabSize,
+    setTabSize,
+    fontSize,
+    setFontSize
+}) {
+  const [isOpen, setIsOpen] = useState(true); 
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+
+  return (
+    <div className="Dropdown">
+    <div className={`${isOpen ? 'darkenButton' : ''}`} onClick={toggleDropdown}>Text Formatting Options</div>
+      {isOpen && (
+        <div className="Options">
+            <ul>
+                <li>Line Numbers <input type="checkbox" checked={showLineNumbers} onChange={() => setShowLineNumbers(prev => !prev)} /></li>
+                <li>Tab Size<input className="num" type="number" value={tabSize} onChange={e => setTabSize(parseInt(e.target.value))} /></li>
+                <li>Font Size<input className="num" type="number" value={fontSize} onChange={e => setFontSize(parseInt(e.target.value))} /></li>
+            </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+
+export default TextFormatingOptions;
