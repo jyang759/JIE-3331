d76a7616180ec56fd8f1ced3b2cbe19a42c37b48
jyang759 <jyang759@gatech.edu>
2024-04-20 20:03:08 -0400

    modified dropdown, language options, saving options, and textformatting options to add hover and highlight features to the big dropdown buttons

--- src/components/LanguageOptions.js
+++ src/components/LanguageOptions.js
@@ -12,36 +12,42 @@ function LanguageOptions ({
     spellChecking,
     setSpellChecking
 }) {
-  const [isOpen, setIsOpen] = useState(true); 
+    const [isOpen, setIsOpen] = useState(true); 
 
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
 
-
-  return (
-    <div className="Dropdown">
-    <div className={`${isOpen ? 'darkenButton' : ''}`} onClick={toggleDropdown}>Language Options</div>
-      {isOpen && (
-        <div className="Options">
-            <ul>
-                <li>Spell Check <input type="checkbox" checked={spellChecking} onChange={() => setSpellChecking(prev => !prev)} /></li>
-                <li>Syntax Highlighting <input type="checkbox" checked={syntaxOn} onChange={() => setSyntaxOn(prev => !prev)} /></li>
-                <li>Auto Detect Language <input type="checkbox" checked={langDetection} onChange={() => setLangDetection(prev => !prev)} /></li>
-                <li className="language-container">
-                <label htmlFor="language" className="language-label">Language</label>
-                    <select id="language" className="language-select" value={selectedLang} onChange={(e) => setSelectedLang(e.target.value)} disabled={langDetection}>
-                        {filteredLangNames.map((lang, index) => (
-                            <option key={index} value={lang}>{lang}</option>
-                        ))}
-                    </select>
-                </li>
-            </ul>
+    return (
+        <div className="Dropdown">
+            <div className={`${isOpen ? 'darkenButton' : ''} LanguageOptionButton`} onClick={toggleDropdown}>
+                Language Options
+            </div>
+            {isOpen && (
+                <div className="Options">
+                    <ul>
+                        <li>
+                            Spell Check <input type="checkbox" checked={spellChecking} onChange={() => setSpellChecking(prev => !prev)} />
+                        </li>
+                        <li>
+                            Syntax Highlighting <input type="checkbox" checked={syntaxOn} onChange={() => setSyntaxOn(prev => !prev)} />
+                        </li>
+                        <li>
+                            Auto Detect Language <input type="checkbox" checked={langDetection} onChange={() => setLangDetection(prev => !prev)} />
+                        </li>
+                        <li className="language-container">
+                            <label htmlFor="language" className="language-label">Language</label>
+                            <select id="language" className="language-select" value={selectedLang} onChange={(e) => setSelectedLang(e.target.value)} disabled={langDetection}>
+                                {filteredLangNames.map((lang, index) => (
+                                    <option key={index} value={lang}>{lang}</option>
+                                ))}
+                            </select>
+                        </li>
+                    </ul>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
-
 
 export default LanguageOptions;
--- src/components/SavingOptions.js
+++ src/components/SavingOptions.js
@@ -7,27 +7,31 @@ function SavingOptions ({
     autosaveTime,
     setAutosaveTime
 }) {
-  const [isOpen, setIsOpen] = useState(true); 
+    const [isOpen, setIsOpen] = useState(true); 
 
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
 
-
-  return (
-    <div className="Dropdown">
-    <div className={`${isOpen ? 'darkenButton' : ''}`} onClick={toggleDropdown}>Save Options</div>
-      {isOpen && (
-        <div className="Options">
-            <ul>
-                <li>Autosave <input type="checkbox" checked={autosaveOn} onChange={() => setAutosaveOn(prev => !prev)} /></li>
-                <li>Autosave Timer <input disabled={!autosaveOn} className="num" type="number" value={autosaveTime} onChange={e => setAutosaveTime(parseInt(e.target.value))} /></li>
-            </ul>
+    return (
+        <div className="Dropdown">
+            <div className={`${isOpen ? 'darkenButton' : ''} SavingOptionButton`} onClick={toggleDropdown}>
+                Save Options
+            </div>
+            {isOpen && (
+                <div className="Options">
+                    <ul>
+                        <li>
+                            Autosave <input type="checkbox" checked={autosaveOn} onChange={() => setAutosaveOn(prev => !prev)} />
+                        </li>
+                        <li>
+                            Autosave Timer <input disabled={!autosaveOn} className="num" type="number" value={autosaveTime} onChange={e => setAutosaveTime(parseInt(e.target.value))} />
+                        </li>
+                    </ul>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
-
 
 export default SavingOptions;
